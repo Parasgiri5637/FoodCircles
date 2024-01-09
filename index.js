a8f8803592adaf8358eaec6439de91c7d86b816a
Parasgiri5637 <86201419+Parasgiri5637@users.noreply.github.com>
2024-01-09 22:53:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ window.addEventListener("scroll", () => {
   navbar.classList.remove("active");
 });
 
-//* this is animation library called anime && it working fine
+//* this is animation library called anime
 //* animation load when page load
 window.addEventListener("DOMContentLoaded", () => {
   anime({
