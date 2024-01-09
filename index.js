793d248e947bf82d7f2fa4b7ae847456f3629d68
Parasgiri5637 <86201419+Parasgiri5637@users.noreply.github.com>
2024-01-09 22:59:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ window.addEventListener("scroll", () => {
 });
 
 //* this is animation library called anime
+//* good animation library
 //* animation load when page load
 window.addEventListener("DOMContentLoaded", () => {
   anime({
